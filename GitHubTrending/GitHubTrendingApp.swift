7363236ab07c4c8d9dbd847aa68f42e533c5ac66
Muhammed Elsayed <Mohamed.Elsayed@bvs-mea.com>
2025-11-15 13:07:07 +0200

    Initial Xcode project setup for GitHubTrending app

---
+++ GitHubTrending/GitHubTrendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTrendingApp.swift
+//  GitHubTrending
+//
+//  Created by Muhammed Elsayed on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTrendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
